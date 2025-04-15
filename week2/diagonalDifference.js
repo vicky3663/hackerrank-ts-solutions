23bfe0fdccc96a7c9964ea515bfb4c3612081a87
Vicky <victorineverkijika@gmail.com>
2025-04-15 12:07:43 +0100

    Diagonal Difference

---
+++ week2/diagonalDifference.js
@@ -0,0 +1,16 @@
+function diagonalDifference(arr) {
+    let primaryDiagonalSum = 0;
+    let secondaryDiagonalSum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        primaryDiagonalSum += arr[i][i];
+        secondaryDiagonalSum += arr[i].reverse()[i];
+    }
+    return Math.abs(primaryDiagonalSum - secondaryDiagonalSum);
+}
+// Example usage:
+const matrix = [
+    [11, 2, 4],
+    [4, 5, 6],
+    [10, 8, -12]
+];
+console.log(diagonalDifference(matrix)); // Output: 15
